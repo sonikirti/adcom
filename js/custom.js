a9362595158266ff58fceca3d4fd53cb9436ebf4
Kirti soni <kirtisoni530@gmail.com>
2014-09-01 13:22:59 +0530

    files

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	
+
+		
+		
+	$('.product-slider').owlCarousel({
+    loop:true,
+    margin:0,
+	rtl:true,
+    responsiveClass:true,
+    items:9,
+    nav:true,			
+	
+	});
+		
+	
+	
+		
+	 });
+	 
+	
+	
+	
+	
+	
---
+++ js/layerslider.js
@@ -0,0 +1,25 @@
+
+// Function Start
+$(window).load(function(){
+
+'use strict';
+
+/* ==============================================
+LAYER SLIDER
+=============================================== */
+
+	jQuery("#layerslider").layerSlider({
+			responsive: true,
+			responsiveUnder: 1093,
+			layersContainer: 1093,
+			skin: 'fullwidth',
+			skinsPath: 'css/layerslider/skins/',
+		});
+
+/* ==============================================
+NAVIGATION MENU - STICKY
+=============================================== */
+
+	
+
+});// End Function
